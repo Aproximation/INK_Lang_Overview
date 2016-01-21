0fbf7f0ddeb31fc6724f2ef0e3adf8e16756dd40
Michał Guzowski <guzowski.michal@hotmail.com>
2016-01-21 14:26:19 +0100

    SP PS update

--- PS_Console/Program.ps1
+++ PS_Console/Program.ps1
@@ -2,10 +2,14 @@
 # Script.ps1
 #
  param (
-	[Parameter(Mandatory = $true)] [String] [ValidateNotNullOrEmpty()] $filePath
+	[Parameter(Mandatory = $true)] [String] [ValidateNotNullOrEmpty()] $filePath #opcjonalnie $filePath = $(throw "Parameter -filePath is required.") // czyli jesli nie podamy parametru, to zostanie mu przypisana wartość throw exception
+
  )
 
  BEGIN{
+ #if((Get-PSSnapin | Where {$_.Name -eq "Microsoft.SharePoint.PowerShell"}) -eq $null) {
+ #   Add-PSSnapin Microsoft.SharePoint.PowerShell;
+ #}
     $ScriptsDir = Resolve-Path .\
 	Import-Module ("{0}\FileControler.psm1" -f $ScriptsDir) -ea Stop
  }
